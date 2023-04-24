56def3a014d4238e85ef59e29092799e0580a2bb
Yan <yan@yanjobs.me>
2023-04-24 16:25:40 +0200

    added the very basics of a passgen
    now locked to 20 & all chars but its just v1

--- PassGen/PassGenApp.swift
+++ PassGen/PassGenApp.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct PassGenApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+    
+  func genPass() {
+    let letters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~0123456789"
+
+    NSPasteboard.general.clearContents()
+    NSPasteboard.general.setString(
+      String((0..<20).map { _ in letters.randomElement()! }),
+      forType: NSPasteboard.PasteboardType.string)
+  }
+    func openGithub () {
+        guard let githubUrl = URL(string: "https://github.com/yan-jobs") else {return}
+        NSWorkspace.shared.open(githubUrl)
     }
+  var body: some Scene {
+      
+    MenuBarExtra {
+        let _ = NSApplication.shared.setActivationPolicy(.prohibited)
+        Label("PassGen", systemImage: "key.horizontal")
+        Button(action: genPass) {
+            Label("Generate", systemImage: "plus.square")
+        }
+        Divider()
+        Button("Made by Yan :)", role: .none, action: openGithub)
+        Button("Quit", role: .destructive) {
+        exit(0)
+      }
+
+    } label: {
+      Label("PassGen", systemImage: "key.viewfinder")
+    }
+  }
 }
+
+
