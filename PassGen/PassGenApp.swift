8d96542b3135d42719e1417092b4822855e3f83d
Yan <yan@yanjobs.me>
2023-04-24 12:17:38 +0200

    Initial Commit

---
+++ PassGen/PassGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassGenApp.swift
+//  PassGen
+//
+//  Created by Yanice :) on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PassGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
